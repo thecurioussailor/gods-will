90dc90d72699dbaf19a8861b07d90c4e159c809e
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-06-18 20:49:01 +0530

    adding chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/lib/polyfills";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <script
+  src="https://relaay-eight.vercel.app//widget.js"
+  data-org-id="45e31b47-e5bd-4c75-881f-d552d0e310ab"
+  data-app-url="https://relaay-eight.vercel.app/"
+></script>
       </body>
     </html>
   );
